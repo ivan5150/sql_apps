ca25f5868aa85956ea352616c5e4edc8a364c685
Ivan Yarosh <rat90@inbox.ru>
2016-02-25 00:15:21 +0300

    without deleting

--- src/main/java/pack/test/sql/Main.java
+++ src/main/java/pack/test/sql/Main.java
@@ -1,5 +1,151 @@
 package pack.test.sql;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String DB_URL = "jdbc:h2:~/test";
+    public static final String LOGIN = "sa";
+    public static final String PASSWORD = "";
+    public static final String COLUMN_AUTHOR = "author";
+    public static final String COLUMN_TEXT = "text";
+    public static final String H2_DRIVHER = "org.h2.Driver";
+    public static final String EXIT = "-stop";
+    public static final String SHOW_ALL = "-all";
+    public static final String CURRENT_AUTHOR = "SELECT * FROM messages WHERE author = ?;";
+    public static final String MESSAGES_FROM_TABLE = "SELECT * FROM messages;";
+    private static final String AUTHOR_ADD_VALUES = "INSERT INTO messages (author, text) VALUES (?, ?);";
+    private static final String DELETE_VALUES = "DELETE FROM messages WHERE author = 'o';";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName(H2_DRIVHER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("Enter name of author or to print her text or '-all' to all table content. For Exit enter '-stop'");
+        try (Scanner sc = new Scanner(System.in);) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (isStopTheApp(line)) {
+                    break;
+                } else if (isPrintAll(line)) {
+                    printAll();
+                } else {
+                    printAuthor(line);
+
+                    String[] authorAndMessage = line.split(":");
+                    if (authorAndMessage.length == 2) {
+                        AddNewUnit(authorAndMessage[0], authorAndMessage[1]);
+                        printAll();
+                     //   DeleteUnit(authorAndMessage[0], authorAndMessage[1]);
+                     //   printAll();
+                    } else {
+                        System.out.println("Incorrect command '" + line + "'");
+                    }
+
+                }
+            }
+        }
+    }
+
+    private static void printAuthor(String line) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(CURRENT_AUTHOR);) {
+            statement.setString(1, line);
+            try (ResultSet resultSet = statement.executeQuery();) {
+                printFetchedMessages(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printAll() {
+        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(MESSAGES_FROM_TABLE);) {
+            printFetchedMessages(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printFetchedMessages(ResultSet resultSet) throws SQLException {
+        System.out.println("Enter something");
+        while (resultSet.next()) {
+            String author = resultSet.getString(COLUMN_AUTHOR);
+            String text = resultSet.getString(COLUMN_TEXT);
+            System.out.println(author + " : " + text);
+        }
+    }
+
+    /*private static void AddNewUnit(String author, String text) {
+        System.out.println("New columns added");
+        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+             PreparedStatement pstm = connection.prepareStatement(AUTHOR_TEXT_VALUES);){
+            pstm.setString(1, author);
+            pstm.setString(1, text);
+            pstm.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }*/
+
+    private static void AddNewUnit(String author, String message) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(AUTHOR_ADD_VALUES);) {
+            statement.setString(1, author);
+            statement.setString(2, message);
+            int rowCount = statement.executeUpdate();
+            if (rowCount == 0) {
+                throw new RuntimeException("INSERT ERROR");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void DeleteUnit(String author, String message) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(DELETE_VALUES);) {
+            statement.setString(1, author);
+            statement.setString(2, message);
+            int rowCount = statement.executeUpdate();
+            if (rowCount == 0) {
+                throw new RuntimeException("INSERT ERROR");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private static boolean isStopTheApp(String line) {
+        return line.equals(EXIT);
+    }
+
+    private static boolean isPrintAll(String line) {
+        return line.equals(SHOW_ALL);
+    }
+}
+
+
+
+/*package pack.test.sql;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -84,7 +230,7 @@ public class Main {
     }
 }
 
-
+*/
 
 /*import java.sql.Connection;
 import java.sql.DriverManager;
